0a38cfab97022067059e32272ec58fa61a239653
SuzanKalugina <123072204+SuzanKalugina@users.noreply.github.com>
2023-02-14 14:19:23 +0300

    ref

--- Homework4/Homework4.Task2/Program.cs
+++ Homework4/Homework4.Task2/Program.cs
@@ -6,3 +6,13 @@ for (var i = min2DigitNumber; i < min3DigitNumber; i += step)
 {
     Console.Write($"{i} \t");
 }
+
+Console.WriteLine();
+
+for (var i = min2DigitNumber; i < min3DigitNumber; i += step)
+{
+    if (i % 5 == 0)
+    {
+        Console.Write($"{i} \t");
+    }
+}
