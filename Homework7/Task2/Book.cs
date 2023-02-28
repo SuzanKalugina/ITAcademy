943da02623964d9473619d5d4407882d441608ac
SuzanKalugina <123072204+SuzanKalugina@users.noreply.github.com>
2023-02-28 14:05:46 +0300

    Homework7

---
+++ Homework7/Task2/Book.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Linq;
+
+namespace Task2
+{
+    internal class Book : IPrintable
+    {
+        public string Name { get; set; }
+        public string СoverСolor { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public Book(string name, string coverСolor, int numberOfPages)
+        {
+            Name = name;
+            СoverСolor = coverСolor;
+            NumberOfPages = numberOfPages;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Это книга. Название: {Name}, цвет обложки: {СoverСolor}, количество страниц: {NumberOfPages}."); 
+        }
+        public static void PrintBooks(IPrintable[] printable)
+        {
+            foreach (var item in printable)
+            {
+                if (item is Book)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+    }
+}
